4193006a5096c7714f330cf260ff85faa4516ec9
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-10-13 08:38:37 +0200

    done
    compare string lenght

---
+++ Open-Lab-03.03/Open-Lab-03.03/Comparator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Open_Lab_03._03
+{
+    public class Comparator
+    {
+        public bool CompareCharactersCount(string str1, string str2)
+        {
+            int l1 = str1.Length;
+            int l2 = str2.Length;
+            if (l1 == l2)
+
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+                }
+            }
+    }
+}
